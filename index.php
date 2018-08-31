f299af985e0f77965e2f9c592517bdc285c141c3
Sadie Sweetman <ssweetm@g.clemson.edu>
2018-08-31 09:15:25 -0400

    Create database connection

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<!DOCTYPE html>
+
+<html>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +9,35 @@
  * Date: 8/31/2018
  * Time: 8:52 AM
  */
+include_once "database.php";
+include_once "navbar.php";
+$db = new DatabaseConnection();
+$user = (empty($_SESSION['username'])) ? "" : $db->connection->real_escape_string($_SESSION['username']);
+?>
+
+<head>
+    <title>MyGDIP</title>
+    <link rel="stylesheet" href="CSS/general.css">
+    <link rel="stylesheet" href="CSS/index.css">
+</head>
+
+<body>
+    <!-- Index Header Content -->
+    <div class = "header">
+        <!--TODO -->
+    </div>
+
+    <!-- Index Page Content -->
+    <div class = "flex-container">
+        <!-- TODO -->
+
+        <!-- Footer Content -->
+        <div class="footer">
+            <h6><b>Brought to you by</b><br><i>Jake Aufschlager, Mitchell Bowser, Patrick Coutu, and Sadie Sweetman</i></h6>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sadie
+ * Date: 8/31/2018
+ * Time: 9:12 AM
+ */
